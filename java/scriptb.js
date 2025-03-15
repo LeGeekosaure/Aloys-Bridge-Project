87429fce497b4c8cf73c9852f8c4c1732fea61c8
LeGeekosaure <enzolombard1306@gmail.com>
2025-03-15 20:11:25 +0100

    retest

--- java/scriptb.js
+++ java/scriptb.js
@@ -32,6 +32,14 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   });
+  document.addEventListener("DOMContentLoaded", function () {
+    const cards = document.querySelectorAll(".card");
+    cards.forEach((card, index) => {
+      setTimeout(() => {
+        card.classList.add("show");
+      }, index * 200);
+    });
+  });
   
   
   
